ce320d928cb3ea7855670a6cada02fe4231cb9c2
insertcustomname <43460834+insertcustomname@users.noreply.github.com>
2019-01-19 17:16:52 +0800

    update

--- main.py
+++ main.py
@@ -10,65 +10,91 @@ import os
 from flask import Flask, request #import main Flask class and request object
 import time
 import sys 
+  
 
-app = Flask(__name__) #create the Flask app
-@app.route('/endpoint')
 def startRecording(name):
     #RUN START RECORDING CODE WITH NAME
     
-    print("Started Recording")   
-
+    print("Started Recording " + name)   
+    return("STARTED")
 def stopRecording():
     #RUN STOP RECORDING CODE
     print("Stopped Recording") 
-def runCode(code):
+def runCode(code,request):
     codeList=code.split("\n")
     for i in codeList:
-        if i.split(" ")[0].lower() == "run":
-            #RUN CODE WITH NAME
-            if i.split(" ")[1].lower() == "0":
-                #RUN CODE ONCE
+        if i.split(" ")[0] == "run":
+            if i.split(" ")[2] == "0":
+                #RUN ONCE 
+                print(i.split(" ")[1].lower())
 
             else:
-                futureTime=time.time()+int(i.split(" ")[2].lower())
-                while futureTime > time.time():
-                    #runcode with name 
-            print("Code to run is " + i.split(" ")[1].lower())
-        elif i.split(" ")[0].lower() == "wait":
-            time.sleep(int(i.split(" ")[1].lower()))
-
-        elif i.split(" ")[1].lower()=="repeat":
+                futureTime=time.time()+int(i.split(" ")[2])
+                while time.time() < futureTime:
+                    #PRINT FOR AS LONG as it goes
+                    print(i.split(" ")[1].lower())
+        elif i.split(" ")[0] == "wait":
+            time.sleep(int(i.split(" ")[1]))
+        elif i.split(" ")[0] == "repeat":
+            amtofrepeats=int(i.split(" ")[1])
             count=0
-            while count < int(i.split(" ")[2].lower()):
-                for i in code.split("\n")[0:len(code.split("\n"))-1]:
-                    if i.split(" ")[0].lower() == "run":
-                            #RUN CODE WITH NAME
-                        if i.split(" ")[1].lower() == "0":
-                            #RUN CODE ONCE
+            codeList.pop()
+            while count < amtofrepeats:
+                
+                for i in codeList:
+                    if i.split(" ")[0] == "run":
+                        if i.split(" ")[2] == "0":
+                            #RUN ONCE 
+                            print(i.split(" ")[1].lower())
 
                         else:
-                            futureTime=time.time()+int(i.split(" ")[2].lower())
-                            while futureTime > time.time():
-                                #runcode with name 
-                        print("Code to run is " + i.split(" ")[1].lower())
-                    elif i.split(" ")[0].lower() == "wait":
-                        time.sleep(int(i.split(" ")[1].lower()))
-
+                            futureTime=time.time()+int(i.split(" ")[2])
+                            while time.time() < futureTime:
+                                #PRINT FOR AS LONG as it goes
+                                print(i.split(" ")[1].lower())
+                            print("done")
+                    elif i.split(" ")[0] == "wait":
+                        time.sleep(int(i.split(" ")[1]))
                 count+=1
-
                     
                     
                 
-            
         
+            
+                
 
-    if request.args.get("record") == "start":
-        return("Starting...")
-        startRecording(request.args.get("name"))
-    elif request.args.get("record") == "stop":
-        return("Stopping...")
-        stopRecording()
+app = Flask(__name__) #create the Flask app
+@app.route('/endpoint',methods = ['POST'])
+def endpoint():
     
-    elif request.args.get("record")=="run":
-        return("Running Code")
-        runCode(requests.args.get("code"))
+    
+    print(str(request.form.get("name")))
+    
+    #print(request.get_json())
+    
+    if request.form.get("record") == "start":
+        
+        startRecording(request.form.get("name"))
+        return("Starting...")
+    elif request.form.get("record") == "stop":
+        
+        stopRecording()
+        return("Stopping...")
+    
+    elif request.form.get("record")=="run":
+        
+        runCode(request.form.get("code"),request)
+        return("Running Code...")
+  
+                    
+
+    
+    
+                
+            
+    
+
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 8080))
+    app.run(host='0.0.0.0', port=port)
